7af9067476234b1ed86c8076355e73d2d968f9f9
Margustova <shchennikova.ma@gmail.com>
2022-10-25 22:58:55 +0200

    HW1

--- Program.cs
+++ Program.cs
@@ -70,16 +70,55 @@ double y2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(Distance(x1, y1, x2, y2));*/
 
 /*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N. */
-void Quadrat(int N)
+// void Quadrat(int N)
+// {
+//     int index =1; // переменная, на которую мы будем увеличивать шаг
+//     while (index <= N)
+//         {
+//             double result = Math.Pow(index,2); //индекс и степень в которую мы его возводим
+//             index++;
+//             Console.Write($"{result} ");
+//         }
+// }
+// Console.Write("Input N : ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// Quadrat(N); 
+
+/*Задача 19
+
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+
+Задача 21
+
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+
+A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+void Table( int N)
 {
-    int index =1; // переменная, на которую мы будем увеличивать шаг
+    int index = 1;
     while (index <= N)
-        {
-            double result = Math.Pow(index,2); //индекс и степень в которую мы его возводим
-            index++;
-            Console.Write($"{result} ");
-        }
+    { 
+    double result = Math.Pow(index, 3);
+    Console.WriteLine(result );
+    index ++;
+    }
 }
-Console.Write("Input N : ");
+Console.WriteLine("Input N: ");
 int N = Convert.ToInt32(Console.ReadLine());
-Quadrat(N); 
+Table(N);
+
